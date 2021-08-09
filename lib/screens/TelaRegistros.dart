d67d829dc9b000c46a4e2627722cdb7f285effbb
Laizorrane <devlaizteixeira@gmail.com>
2021-08-09 16:11:01 -0300

    Adição de novas funcionalidades ao usuario

--- lib/screens/TelaRegistros.dart
+++ lib/screens/TelaRegistros.dart
@@ -126,6 +126,7 @@ class _TelaRegistrosState extends State<TelaRegistros>
                                   padding:
                                       const EdgeInsets.only(left: 16, right: 16),
                                   child: ListView.builder(
+                                    
                                     reverse: true,
                                     primary: false,
                                     itemBuilder: (context, index) {
